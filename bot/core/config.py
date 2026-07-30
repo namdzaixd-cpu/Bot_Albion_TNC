a3be36ba7eba98d8f29984e3bc5edd2ba97ab0c6
TwotNguyenVN <nguyenngoctinh011258@gmail.com>
2026-07-31 03:49:28 +0700

    feat: tự động nạp file .env cục bộ qua python-dotenv

--- bot/core/config.py
+++ bot/core/config.py
@@ -1,6 +1,12 @@
 import os
 import random
 
+try:
+    from dotenv import load_dotenv
+    load_dotenv()
+except ImportError:
+    pass
+
 TOKEN = os.getenv("DISCORD_TOKEN", "")
 GIT_URL = os.getenv("GITHUB_GIT_URL", "")
 GUILD_ID = int(os.getenv("DISCORD_GUILD_ID", "712258265769050164"))
